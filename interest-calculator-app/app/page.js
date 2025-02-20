686e8bec5656dde38416afb2a3a29ec21b500a33
Luca Battesini <lucabattesini1@gmail.com>
2025-02-20 10:32:33 -0500

    Organizing new inputs to the new function

--- interest-calculator-app/app/page.js
+++ interest-calculator-app/app/page.js
@@ -3,9 +3,11 @@ import Form from 'next/form'
 export default function MainPage() {
   return (
     <main>
-      <h1>Hello world!</h1>
       <Form action="/search">
-      <input name="Anos"/>
+      <input name="Initial investment"/>
+      <input name="Monthly investment"/>
+      <input name="Aplication period"/>
+      <input name="Interest rate"/>
       <button type='submit'>Submit</button>
       </Form>
     </main>
