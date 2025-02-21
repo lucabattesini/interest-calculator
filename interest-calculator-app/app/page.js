c72a48c12b2996916076b29f42693148a0018964
Luca Battesini <lucabattesini1@gmail.com>
2025-02-21 10:37:03 -0500

    recreating the inputs, now in a correc way

--- interest-calculator-app/app/page.js
+++ interest-calculator-app/app/page.js
@@ -1,17 +1,15 @@
-import Form from 'next/form'
+
 
 export default function MainPage() {
   return (
     <main>
       <div className="mainDiv">
-      <h1>Investment calculator</h1>
-      <Form action="/search">
-      <input name="Initial investment"/>
-      <input name="Monthly investment"/>
-      <input name="Aplication period"/>
-      <input name="Interest rate"/>
-      <button type='submit'>Submit</button>
-      </Form>
+        Initial Investment
+        <br/>
+        <input className="initialInvestment"></input>
+        <input className="monthlyInvestment"></input>
+        <input className="aplicationPeriod"></input>
+        <input className="interestRate"></input>
       </div>
     </main>
   );
