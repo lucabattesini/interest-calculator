463d5bd2a4dfd8e4c1b57ca5063907d78cc19159
Luca Battesini <lucabattesini1@gmail.com>
2025-02-21 11:22:51 -0500

    Adding a body and a title to the page

--- interest-calculator-app/app/page.js
+++ interest-calculator-app/app/page.js
@@ -2,15 +2,18 @@
 
 export default function MainPage() {
   return (
-    <main>
-      <div className="mainDiv">
-        Initial Investment
-        <br/>
-        <input className="initialInvestment"></input>
-        <input className="monthlyInvestment"></input>
-        <input className="aplicationPeriod"></input>
-        <input className="interestRate"></input>
-      </div>
-    </main>
+    <body>
+      <main>
+        <div className="mainDiv">
+          <h1>Investment calculator</h1>
+          Initial Investment
+          <br/>
+          <input className="initialInvestment"></input>
+          <input className="monthlyInvestment"></input>
+          <input className="aplicationPeriod"></input>
+          <input className="interestRate"></input>
+        </div>
+      </main>
+    </body>
   );
 }
