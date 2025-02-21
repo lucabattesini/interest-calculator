d00bce23c28823ec180a0a4c555c0526bdea7cc3
Luca Battesini <lucabattesini1@gmail.com>
2025-02-21 18:23:16 -0500

    Just adding labels and placeholders to the other inputs

--- interest-calculator-app/app/page.js
+++ interest-calculator-app/app/page.js
@@ -12,13 +12,16 @@ export default function MainPage() {
             <div className="firstLine">
               <label for="investment">Initial investment</label>
               <input className="initialInvestment" placeholder="Type"></input>
-              
+
               <label for="investment">Monthly investment</label>
-              <input className="monthlyInvestment"></input>
+              <input className="monthlyInvestment" placeholder="Type"></input>
             </div>
             <div className="secondLine">
-              <input className="aplicationPeriod"></input>
-              <input className="interestRate"></input>
+              <label for="investment">Aplication period</label>
+              <input className="aplicationPeriod" placeholder="Type"></input>
+              
+              <label for="investment">Interest rate</label>
+              <input className="interestRate" placeholder="Type"></input>
             </div>
           </div>
         </div>
