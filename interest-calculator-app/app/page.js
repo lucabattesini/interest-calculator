5cb239307c52e20c0798cd60dfb0fcde23b66487
Luca Battesini <lucabattesini1@gmail.com>
2025-02-21 11:33:13 -0500

    Organizing the elements in divs

--- interest-calculator-app/app/page.js
+++ interest-calculator-app/app/page.js
@@ -5,13 +5,17 @@ export default function MainPage() {
     <body>
       <main>
         <div className="mainDiv">
-          <h1>Investment calculator</h1>
-          Initial Investment
-          <br/>
-          <input className="initialInvestment"></input>
-          <input className="monthlyInvestment"></input>
-          <input className="aplicationPeriod"></input>
-          <input className="interestRate"></input>
+          <div className="header">
+            <h1>Investment calculator</h1>
+          </div>
+          <div className="components">
+            Initial Investment
+            <br/>
+            <input className="initialInvestment"></input>
+            <input className="monthlyInvestment"></input>
+            <input className="aplicationPeriod"></input>
+            <input className="interestRate"></input>
+          </div>
         </div>
       </main>
     </body>
