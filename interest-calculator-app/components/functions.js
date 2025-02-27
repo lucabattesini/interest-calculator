dc3c6aa5fed3fb49fe7568196d083da7da4e14d1
Luca Battesini <lucabattesini1@gmail.com>
2025-02-27 10:02:22 -0500

    Creating the body of the main function

--- interest-calculator-app/components/functions.js
+++ interest-calculator-app/components/functions.js
@@ -1,3 +1,16 @@
-export default function InterestCalculation(InitialInvestment, MonthlyInvestment, AplicationPeriod, InterestRate) {
-    
+export default function InterestCalculation(InitialInvestment, MonthlyInvestment, 
+    AplicationPeriod, InterestRate) {
+
+    const repetitions = AplicationPeriod
+    let monthlyResult = []
+    let interest = 0
+
+    for (let i = 0; i < repetitions; i++) {
+        interest += MonthlyInvestment 
+        interest = InitialInvestment * InterestRate * 1
+
+        monthlyResult.push(interest)
+    }
+
+    console.log(monthlyResult, interest)
 }
