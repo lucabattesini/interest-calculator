fd68281a1f20bc9a7fc05e5433bcddec0a0addbe
Luca Battesini <lucabattesini1@gmail.com>
2025-02-27 09:14:02 -0500

    Adding a number type to the inputs

--- interest-calculator-app/app/page.js
+++ interest-calculator-app/app/page.js
@@ -11,17 +11,17 @@ export default function MainPage() {
           <div className="components">
             <div className="firstLine">
               <label for="investment">Initial investment</label>
-              <input className="initialInvestment" placeholder="0,00"></input>
+              <input className="initialInvestment" type="number" placeholder="0,00"></input>
 
               <label for="investment">Monthly investment</label>
-              <input className="monthlyInvestment" placeholder="0,00"></input>
+              <input className="monthlyInvestment" type="number" placeholder="0,00"></input>
             </div>
             <div className="secondLine">
               <label for="investment">Aplication period</label>
-              <input className="aplicationPeriod" placeholder="0"></input>
+              <input className="aplicationPeriod" type="number" placeholder="0"></input>
               
               <label for="investment">Interest rate</label>
-              <input className="interestRate" placeholder="0,00%"></input>
+              <input className="interestRate" type="number" placeholder="0,00%"></input>
             </div>
           </div>
           <div className="submitButton">
