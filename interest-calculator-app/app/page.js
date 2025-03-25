0c514e9214c6cdde5d2380baf3da5a727ec5cdfc
Luca Battesini <lucabattesini1@gmail.com>
2025-03-25 10:31:37 -0400

    Making a test of printing the first input in the screen

--- interest-calculator-app/app/page.js
+++ interest-calculator-app/app/page.js
@@ -1,4 +1,4 @@
-
+// Indica que esse é um componente de cliente
 'use client';
 
 // Import the main function
@@ -10,12 +10,15 @@ export default function MainPage() {
   // Onclick functions
 
   // Declare the the main input vars
-  const [initialInvestment, setInitialInvestment] = useState(0)
-  const [monthlyInvestment, setMonthlyInvestment] = useState(0)
-  const [aplicationPeriod, setAplicationPeriod] = useState(0)
-  const [interestRate, setInterestRate] = useState(0)
+  const [initialInvestment, setInitialInvestment] = useState(0);
+  const [monthlyInvestment, setMonthlyInvestment] = useState(0);
+  const [aplicationPeriod, setAplicationPeriod] = useState(0);
+  const [interestRate, setInterestRate] = useState(0);
 
-  
+  // Vai ser chamada quando o usuário digitar o input
+  const handleChange = (event) => {
+    setInitialInvestment(event.target.value); // Atualiza o estado da const com o valor do input
+  };
 
   // Main page html
   return (
@@ -24,14 +27,18 @@ export default function MainPage() {
           <div className="header">
             <h1>Investment calculator</h1>
           </div>
+
           <div className="components">
             <div className="firstLine">
               <label>Initial investment</label>
-              <input className="initialInvestment" type="number" placeholder="0,00"></input>
+              <input className="initialInvestment" placeholder="0,00" 
+              type="number" value={initialInvestment} onChange={handleChange}/>
+              <p> Seu numero é {initialInvestment}</p>
 
               <label>Monthly investment</label>
               <input className="monthlyInvestment" type="number" placeholder="0,00"></input>
             </div>
+
             <div className="secondLine">
               <label>Aplication period</label>
               <input className="aplicationPeriod" type="number" placeholder="0"></input>
@@ -40,6 +47,7 @@ export default function MainPage() {
               <input className="interestRate" type="number" placeholder="0,00%"></input>
             </div>
           </div>
+
           <div className="submitButton">
               <button>Calculate</button>
           </div>
